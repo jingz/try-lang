b8244a3d36e4a217552e191a2d94a9621b9f7821
sarunyoo wangwong <jing@sarunyoos-MacBook-Pro.local>
2014-12-06 12:30:11 +0700

    tidyup

--- coffeescript/cc.rb
+++
@@ -1,9 +0,0 @@
-require 'coffee_script'
-
-scriptname = ARGV[0]
-base_dir = File.dirname scriptname
-compiled_script = scriptname.gsub('coffee','js') || ARGV[1]
-
-File.open("#{base_dir}/#{compiled_script}",'w') do |f|
-  f.write CoffeeScript.compile(File.read("#{base_dir}/#{scriptname}"))
-end
